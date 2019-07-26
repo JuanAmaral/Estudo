7871b11e62bd46bc1acea61285a7414d93ed9c71
huoan <lgdfjktg@gmail.com>
2019-07-25 21:47:44 -0300

    Aula 3

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaBruno = new ContaCorrente();
+            contaBruno.titular = "Bruno";
+
+            Console.WriteLine(contaBruno.saldo);
+            contaBruno.Sacar(50);
+            Console.WriteLine(contaBruno.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
